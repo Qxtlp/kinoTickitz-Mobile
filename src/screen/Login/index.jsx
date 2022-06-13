64728d5a41c26c6616f88eb9b6dc2ce3439982fb
Qxtlp <xi.tulop@yandex.com>
2022-06-13 22:34:19 +0700

    login integration

--- src/screen/Login/index.jsx
+++ src/screen/Login/index.jsx
@@ -1,20 +1,51 @@
-import React from 'react';
-import {View, Text, TextInput, TouchableOpacity, Button} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  ActivityIndicator,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import Styles from '../../styles/auth';
 import Layout from '../../components/layout/auth';
+import axios from '../../utils/axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 function LoginScreen(props) {
-  const handleLogin = () => {
-    props.navigation.navigate('AppScreen', {
-      screen: 'Home',
-    });
+  const [form, setForm] = useState({email: '', password: ''});
+  const [isSecure, setIsSecure] = useState(true);
+  const [isError, setIsError] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleLogin = async () => {
+    try {
+      setIsLoading(true);
+      const result = await axios.post('auth/login', form);
+      await AsyncStorage.multiSet(
+        [
+          ['id', result.data.data.id],
+          ['token', result.data.data.token],
+          ['refreshToken', result.data.data.refreshToken],
+        ],
+        err => console.log(err),
+      );
+      setIsLoading(false);
+
+      props.navigation.navigate('AppScreen', {
+        screen: 'Home',
+      });
+    } catch (error) {
+      setIsError(error.response.data.msg);
+      setIsLoading(false);
+    }
   };
 
-  const handleRegister = () => {
-    console.log('object');
-    props.navigation.navigate('Register');
+  const handleChange = (value, name) => {
+    setForm({...form, [name]: value});
+    setIsError('');
   };
+
   return (
     <Layout
       title="Sign In"
@@ -26,8 +57,9 @@ function LoginScreen(props) {
         style={Styles.input}
         placeholder="Write your Email"
         textContentType="emailAddress"
-        // onChangeText={onChangeText}
-        // value={text}
+        keyboardType="email-address"
+        autoCapitalize="none"
+        onChangeText={value => handleChange(value, 'email')}
         autoFocus
       />
       <Text style={{marginTop: 20}}>Password</Text>
@@ -36,28 +68,53 @@ function LoginScreen(props) {
           style={{flex: 2}}
           placeholder="Write your password"
           textContentType="password"
-          secureTextEntry={true}
-          // onChangeText={onChangeText}
-          // value={text}
+          secureTextEntry={isSecure}
+          onChangeText={value => handleChange(value, 'password')}
         />
-        <Icon.Button
-          name="eye"
-          backgroundColor={'hsla(210, 50%, 99%, 1)'}
-          color="grey"
-          style={{flex: 1, paddingHorizontal: 0}}></Icon.Button>
+        <View style={{paddingTop: 10, paddingHorizontal: 5}}>
+          <Icon
+            name={isSecure ? 'eye' : 'eye-off'}
+            backgroundColor={'hsla(210, 50%, 99%, 1)'}
+            color="grey"
+            size={25}
+            style={{flex: 1, paddingHorizontal: 0}}
+            onPress={() => setIsSecure(!isSecure)}></Icon>
+        </View>
       </View>
+
+      {isError ? (
+        <View
+          style={{
+            backgroundColor: 'red',
+            borderRadius: 5,
+            marginTop: 30,
+            padding: 10,
+          }}>
+          <Text style={{textAlign: 'center', color: 'white'}}>{isError}</Text>
+        </View>
+      ) : (
+        <></>
+      )}
 
       <TouchableOpacity
         style={{
-          backgroundColor: '#5F2EEA',
+          backgroundColor:
+            form.email && form.password
+              ? 'hsla(256, 82%, 55%, 1)'
+              : 'hsla(256, 82%, 55%, 0.5)',
           borderRadius: 10,
-          marginTop: 50,
+          marginTop: 30,
           padding: 20,
           color: 'white',
         }}
-        onPress={handleLogin}>
+        onPress={handleLogin}
+        disabled={isLoading || !(form.email && form.password)}>
         <Text style={{textAlign: 'center', color: 'white', fontSize: 16}}>
-          Sign In
+          {isLoading ? (
+            <ActivityIndicator size="small" color="white" />
+          ) : (
+            'Sign In'
+          )}
         </Text>
       </TouchableOpacity>
 
