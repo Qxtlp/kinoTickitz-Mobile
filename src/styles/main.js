094d58b9e9770f75faa37c34bb2289c95853ac69
Qxtlp <xi.tulop@yandex.com>
2022-06-16 12:28:04 +0700

    add app screens styling

---
+++ src/styles/main.js
@@ -0,0 +1,39 @@
+import {StyleSheet} from 'react-native';
+
+const color = {
+  primary: '#5F2EEA',
+};
+
+module.exports = StyleSheet.create({
+  bgMain: {
+    backgroundColor: color.primary,
+    color: 'white',
+  },
+  textMain: {
+    color: color.primary,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    paddingHorizontal: 15,
+  },
+  title: {
+    color: color.primary,
+    fontWeight: 'bold',
+    fontSize: 25,
+    marginVertical: 15,
+  },
+  input: {
+    borderColor: 'hsla(0, 0%, 87%, 0.6)',
+    borderStyle: 'solid',
+    borderWidth: 2,
+    borderRadius: 5,
+    backgroundColor: 'hsla(210, 50%, 99%, 1)',
+    padding: 15,
+    marginTop: 5,
+  },
+  button: {
+    color: 'white',
+    backgroundColor: color.primary,
+  },
+});
