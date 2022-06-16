db46b271f8010c5edc9880a56a8889f4b852a9a9
Qxtlp <xi.tulop@yandex.com>
2022-06-16 12:26:33 +0700

    view all integration

--- src/screen/ViewAll/index.jsx
+++ src/screen/ViewAll/index.jsx
@@ -1,22 +1,27 @@
-import React from 'react';
-import Styles from '../../styles/main';
+import React, {useEffect, useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
 import Layout from '../../components/layout/main';
 import Card from '../../components/card';
 import {
   Box,
-  Center,
-  Container,
-  Heading,
+  FlatList,
+  HStack,
   Input,
   ScrollView,
   Select,
+  Spinner,
   Text,
   View,
 } from 'native-base';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {TouchableHighlight} from 'react-native';
+import {getAllMovie} from '../../stores/action/movie';
 
 function ViewAll(props) {
+  const dispatch = useDispatch();
+  const [month, setMonth] = useState('');
+  const [limit, setLimit] = useState(4);
+  const [sort, setSort] = useState('');
   const monthList = [
     {index: 1, name: 'January'},
     {index: 2, name: 'February'},
@@ -31,10 +36,28 @@ function ViewAll(props) {
     {index: 11, name: 'November'},
     {index: 12, name: 'December'},
   ];
-  console.log('viewall all');
-  return (
-    <Layout>
-      <View bg="custom.2" py={10}>
+
+  const movie = useSelector(state => state.movie);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  useEffect(() => {
+    getData();
+  }, [month, limit, sort]);
+
+  const getData = () => {
+    try {
+      dispatch(getAllMovie(1, limit, '', '', month));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const headerComponents = () => {
+    return (
+      <View back py={5}>
         <View mx={5} centerContent={false}>
           <Text>List Movie</Text>
           <View flexDirection={'row'} mt={2} h={9}>
@@ -49,19 +72,16 @@ function ViewAll(props) {
               borderRadius={'full'}
               bg="white"
               h={1}
-              //   onValueChange={(itemValue) => setLanguage(itemValue)}
-            >
-              <Select.Item label="Wallet" value="key0" />
-              <Select.Item label="ATM Card" value="key1" />
-              <Select.Item label="Debit Card" value="key2" />
-              <Select.Item label="Credit Card" value="key3" />
-              <Select.Item label="Net Banking" value="key4" />
+              onValueChange={itemValue => setSort(itemValue)}>
+              <Select.Item label="Sort" value="" />
+              <Select.Item label="Name" value="name asc" />
             </Select>
             <Input
               flex={2}
               bg={'white'}
-              mx="3"
-              placeholder="Input"
+              ml="3"
+              w={'full'}
+              placeholder="Search movie name"
               variant={'rounded'}
             />
           </View>
@@ -76,6 +96,7 @@ function ViewAll(props) {
                   style={{
                     width: 127,
                     height: 42,
+                    backgroundColor: v.index == month ? '#5F2EEA' : 'white',
                     borderWidth: 1,
                     borderColor: '#5F2EEA',
                     borderRadius: 3,
@@ -83,33 +104,65 @@ function ViewAll(props) {
                     marginStart: 20,
                   }}
                   onPress={() => {
-                    alert(v.name);
+                    setMonth(v.index);
+                    if (v.index == month) setMonth('');
                   }}>
-                  <Text style={{color: '#5F2EEA', textAlign: 'center'}}>
+                  <Text
+                    style={{
+                      color: v.index != month ? '#5F2EEA' : 'white',
+                      textAlign: 'center',
+                    }}>
                     {v.name}
                   </Text>
                 </TouchableHighlight>
               );
             })}
           </ScrollView>
-          <View
-            mt={10}
-            flexDirection="row"
-            flexWrap={'wrap'}
-            justifyContent="space-between">
-            {[1, 2, 3].map(v => {
-              return (
-                <Card.MovieV2
-                  style={{marginTop: 15}}
-                  key={v}
-                  onPress={() =>
-                    props.navigation.navigate('Detail', {id: 'id dasuwa'})
-                  }></Card.MovieV2>
-              );
-            })}
-          </View>
         </View>
       </View>
+    );
+  };
+
+  const footerComponents = () => {
+    return (
+      <Layout>
+        {limit < movie.pageInfo.allMovie ? (
+          <HStack space={3} justifyContent="center" h={100}>
+            <Spinner size={'lg'} color="indigo.500" />
+          </HStack>
+        ) : (
+          <Text my={10} color="gray.500" textAlign="center" fontSize="lg">
+            All Movie already displayed
+          </Text>
+        )}
+      </Layout>
+    );
+  };
+
+  return (
+    <Layout isNotScroll={true}>
+      <FlatList
+        data={movie.allData}
+        numColumns={2}
+        ListHeaderComponent={headerComponents}
+        columnWrapperStyle={{flex: 1, justifyContent: 'space-evenly'}}
+        onEndReached={() =>
+          limit < movie.pageInfo.allMovie ? setLimit(limit + 4) : null
+        }
+        onEndReachedThreshold={0.5}
+        renderItem={({item}) => (
+          <Card.MovieV2
+            style={{marginVertical: 10}}
+            id={item.id}
+            name={item.name}
+            category={item.category}
+            image={item.image}
+            onPress={() =>
+              props.navigation.navigate('Detail', {id: item.id})
+            }></Card.MovieV2>
+        )}
+        ListFooterComponent={footerComponents}
+      />
     </Layout>
   );
 }
