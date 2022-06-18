9c7aff31c7dca59f80884e8c8f387dd22fc3b426
Qxtlp <xi.tulop@yandex.com>
2022-06-18 23:31:32 +0700

    order screen integration

--- src/screen/Order/index.jsx
+++ src/screen/Order/index.jsx
@@ -1,8 +1,5 @@
-import React from 'react';
-import Styles from '../../styles/main';
+import React, {useEffect, useState} from 'react';
 import Layout from '../../components/layout/main';
-import Card from '../../components/card';
-import {DateTimePickerAndroid} from '@react-native-community/datetimepicker';
 import {
   Box,
   Button,
@@ -20,41 +17,87 @@ import {
   View,
 } from 'native-base';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import {useDispatch} from 'react-redux';
+import {getSeats} from '../../stores/action/schedule';
 
 function Order(props) {
-  console.log(props.route.params?.id);
+  const dispatch = useDispatch();
+  const [selectedSeat, setSelectedSeat] = useState([]);
+  const [reservedSeat, setReservedSeat] = useState([]);
+
+  const orderData = props.route.params;
+  useEffect(() => {
+    dispatch(
+      getSeats(orderData.id, orderData.bookingTime, orderData.dateBooking),
+    )
+      .then(res => {
+        setReservedSeat(
+          res.value.data.data.map(item => item.seat.split(',')).flat(),
+        );
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  const handleSelectSeat = seat => {
+    if (selectedSeat.includes(seat)) {
+      const deleteSeat = selectedSeat.filter(i => i !== seat);
+      setSelectedSeat(deleteSeat);
+    } else {
+      setSelectedSeat([...selectedSeat, seat]);
+    }
+  };
+
+  const dataPayment = {
+    date: orderData.dateBooking,
+    time: orderData.bookingTime,
+    movie: orderData.name,
+    cinemaName: orderData.premiere,
+    seat: selectedSeat,
+    total: selectedSeat.length * orderData.price,
+    schedule: orderData.scheduleId,
+  };
 
   return (
     <Layout>
       <View bg={'custom.2'} px="4" py={5}>
         <Text fontSize={'lg'}>Choose Your Seat</Text>
         <Box bg="white" mt={5} py={10} rounded="lg">
-          <Divider
-            w={300}
-            h={'1.5'}
-            mx="auto"
-            borderRadius={'full'}
-            bg={'indigo.400'}
-          />
-          <Flex direction="row" mt={3}>
+          <View px={3}>
+            <Divider
+              w={'full'}
+              h={'1.5'}
+              borderRadius={'full'}
+              bg={'indigo.500'}
+            />
+          </View>
+          <Flex direction="row" mt={3} mx="auto">
             <Divider
               bg="emerald.500"
-              thickness="2"
-              mx="2"
+              thickness="1.5"
+              mx="1"
               h={140}
               orientation="vertical"
             />
             {[1, 2, 3, 4, 5, 6, 7].map(num => {
               return (
-                <Flex>
+                <Flex key={num}>
                   {['A', 'B', 'C', 'D', 'E', 'F', 'G'].map(v => {
+                    console.log(selectedSeat.includes(`${num}${v}`));
                     return (
                       <Pressable
-                        p={2}
+                        key={v}
+                        p={'2'}
                         m={0.5}
-                        bg="blueGray.200"
+                        disabled={reservedSeat.includes(v + num)}
+                        bg={
+                          reservedSeat.includes(v + num)
+                            ? 'gray.400'
+                            : selectedSeat.includes(v + num)
+                            ? 'indigo.500'
+                            : 'blueGray.200'
+                        }
                         rounded={'sm'}
-                        onPress={() => alert(v + num)}>
+                        onPress={() => handleSelectSeat(v + num)}>
                         {/* <Text>{num + v}</Text> */}
                       </Pressable>
                     );
@@ -67,15 +110,20 @@ function Order(props) {
             <View flexDirection={'row'} ml={4}>
               {[8, 9, 10, 11, 12, 13, 14].map(num => {
                 return (
-                  <Flex>
+                  <Flex key={num}>
                     {['A', 'B', 'C', 'D', 'E', 'F', 'G'].map(v => {
                       return (
                         <Pressable
+                          key={v}
                           p={2}
                           m={0.5}
-                          bg="blueGray.200"
+                          bg={
+                            selectedSeat.includes(v + num)
+                              ? 'indigo.500'
+                              : 'blueGray.200'
+                          }
                           rounded={'sm'}
-                          onPress={() => alert(v + num)}>
+                          onPress={() => handleSelectSeat(v + num)}>
                           {/* <Text>{num + v}</Text> */}
                         </Pressable>
                       );
@@ -141,7 +189,7 @@ function Order(props) {
                 Hiflix Cinema
               </Text>
               <Text color={'black'} mt={1} px="10" textAlign={'center'}>
-                Hiflix Cinema
+                {orderData.name}
               </Text>
             </Center>
             <View px={5}>
@@ -149,22 +197,29 @@ function Order(props) {
                 flexDirection={'row'}
                 justifyContent={'space-between'}
                 mt="3">
-                <Text color={'gray.400'}>Price</Text>
-                <Text>00.00/seat</Text>
+                <Text color={'gray.400'}>
+                  {new Date(orderData.dateBooking).toDateString()}
+                </Text>
+                <Text>{orderData.bookingTime}</Text>
               </View>
               <View
                 flexDirection={'row'}
                 justifyContent={'space-between'}
                 mt="3">
-                <Text color={'gray.400'}>Price</Text>
-                <Text>00.00/seat</Text>
+                <Text color={'gray.400'}>One ticket price</Text>
+                <Text>
+                  Rp.
+                  {orderData.price
+                    .toString()
+                    .replace(/\B(?=(\d{3})+(?!\d))/g, '.')}
+                </Text>
               </View>
               <View
                 flexDirection={'row'}
                 justifyContent={'space-between'}
                 mt="3">
-                <Text color={'gray.400'}>Price</Text>
-                <Text>00.00/seat</Text>
+                <Text color={'gray.400'}>Seat choosen</Text>
+                <Text>{selectedSeat.join(',') || '{none}'}</Text>
               </View>
             </View>
             <Divider mt={10} />
@@ -174,11 +229,22 @@ function Order(props) {
               mt="5"
               px={5}>
               <Text>Price</Text>
-              <Text>00.00/seat</Text>
+              <Text color={'indigo.600'} bold="true">
+                Rp.
+                {(orderData.price * selectedSeat.length)
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, '.')}
+              </Text>
             </View>
           </Box>
 
-          <Button w={'full'} mt={10} colorScheme="indigo" shadow={5}>
+          <Button
+            w={'full'}
+            mt={10}
+            colorScheme="indigo"
+            shadow={5}
+            disabled={selectedSeat.length ? false : true}
+            onPress={() => props.navigation.navigate('Payment', dataPayment)}>
             Checkout now
           </Button>
         </Center>
