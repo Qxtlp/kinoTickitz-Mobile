4f218dab0c372f32e91497abff8aba88722a4822
Qxtlp <xi.tulop@yandex.com>
2022-06-21 11:38:47 +0700

    add reset password route

--- src/navigation/auth.js
+++ src/navigation/auth.js
@@ -5,6 +5,9 @@ const Stack = createNativeStackNavigator();
 
 import Login from '../screen/Login';
 import Register from '../screen/Register';
+import Reset from '../screen/Reset';
+import VerifyOTP from '../screen/VerifyOTP';
+import SetPassword from '../screen/SetPassword';
 
 export default function AuthNavigator() {
   return (
@@ -16,9 +19,34 @@ export default function AuthNavigator() {
           headerShown: false,
         }}
       />
+
       <Stack.Screen
         component={Register}
         name="Register"
+        options={{
+          headerShown: false,
+        }}
+      />
+
+      <Stack.Screen
+        component={Reset}
+        name="Reset"
+        options={{
+          headerShown: false,
+        }}
+      />
+
+      <Stack.Screen
+        component={VerifyOTP}
+        name="VerifyOTP"
+        options={{
+          headerShown: false,
+        }}
+      />
+
+      <Stack.Screen
+        component={SetPassword}
+        name="SetPassword"
         options={{
           headerShown: false,
         }}
