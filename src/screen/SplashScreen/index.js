5cf6261437631787d4c72d9dcd816e7d33fc925c
Qxtlp <xi.tulop@yandex.com>
2022-06-21 11:40:03 +0700

    change splashscreen implementation

--- src/screen/SplashScreen/index.js
+++ src/screen/SplashScreen/index.js
@@ -1,17 +1,20 @@
 /* eslint-disable react-native/no-inline-styles */
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, {useEffect} from 'react';
 import {View, Text} from 'react-native';
 
 function SplashScreen(props) {
   useEffect(() => {
-    const token = false;
-    setTimeout(() => {
+    (async () => {
+      const token = await AsyncStorage.getItem('token');
+      console.log(token);
+
       if (token) {
         props.navigation.navigate('AppScreen');
       } else {
         props.navigation.navigate('AuthScreen');
       }
-    }, 1000);
+    })();
   }, []);
 
   return (
