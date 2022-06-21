fe4c15a53b26c0ca71d8a333996e007a58549bcc
Qxtlp <xi.tulop@yandex.com>
2022-06-21 11:36:37 +0700

    remove unneccessary code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,10 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text, TouchableWithoutFeedback} from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import Styles from '../styles/main';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Header(props) {
-  const [data, setData] = useState({});
-  const result = AsyncStorage.multiGet(['id', 'token'], (err, stores) => {
-    if (err) {
-      console.log(err);
-    }
-    console.log(stores.map(result => result));
-  });
   return (
     <View style={{backgroundColor: 'white', paddingHorizontal: 16}}>
       <View
