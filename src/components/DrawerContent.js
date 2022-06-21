6a4309f7b2e5765f474265cca61d5f148877abc3
Qxtlp <xi.tulop@yandex.com>
2022-06-21 11:36:03 +0700

    drawer integration

--- src/components/DrawerContent.js
+++ src/components/DrawerContent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import {
   DrawerContentScrollView,
@@ -9,8 +9,25 @@ import {
 import Icon from 'react-native-vector-icons/Feather';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useDispatch, useSelector} from 'react-redux';
+import {getUser} from '../stores/action/user';
+import {Image, NativeBaseProvider} from 'native-base';
+import Config from 'react-native-config';
 
 function DrawerContent(props) {
+  const dispatch = useDispatch();
+  let image = `${Config.IMG_URL}default-profile.png`;
+  const user = useSelector(state => state.user);
+  if (user.data.image) {
+    image = `${Config.IMG_URL}${user.data.image}`;
+  }
+  useEffect(() => {
+    (async () => {
+      const id = await AsyncStorage.getItem('id');
+      dispatch(getUser(id));
+    })();
+  }, []);
+
   const handleLogout = async () => {
     try {
       alert('Logout');
@@ -20,34 +37,47 @@ function DrawerContent(props) {
       });
     } catch (error) {}
   };
+
   return (
-    <View style={styles.container}>
-      <DrawerContentScrollView {...props}>
-        <View style={styles.containerProfile}>
-          <View style={styles.avatar} />
-          <View style={styles.biodata}>
-            <Text style={styles.title}>Anonymous</Text>
-            <Text style={styles.caption}>@bagustea</Text>
+    <NativeBaseProvider>
+      <View style={styles.container}>
+        <DrawerContentScrollView {...props}>
+          <View style={styles.containerProfile}>
+            <Image
+              source={{uri: image}}
+              size={'16'}
+              my={5}
+              borderRadius={100}
+              alt="profile"
+            />
+            <View style={styles.biodata}>
+              <Text
+                style={
+                  styles.title
+                }>{`${user.data.firstName} ${user.data.lastName}`}</Text>
+              <Text style={styles.caption}>{user.data.email}</Text>
+            </View>
           </View>
+          <DrawerItemList {...props} />
+        </DrawerContentScrollView>
+        <View style={styles.containerSection}>
+          <DrawerItem
+            label="Logout"
+            icon={({color, size}) => (
+              <Icon color={color} size={size} name="log-out" />
+            )}
+            onPress={handleLogout}
+          />
         </View>
-        <DrawerItemList {...props} />
-      </DrawerContentScrollView>
-      <View style={styles.containerSection}>
-        <DrawerItem
-          label="Logout"
-          icon={({color, size}) => (
-            <Icon color={color} size={size} name="log-out" />
-          )}
-          onPress={handleLogout}
-        />
       </View>
-    </View>
+    </NativeBaseProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'white',
   },
   containerProfile: {
     margin: 10,
